0348d2fe1c1e53a6139157ca2d2cc83d11fc80f7
Marissa D'Alonzo <dalonzo.m@husky.neu.edu>
2018-11-11 09:56:35 -0500

    notes

--- breathing_exercise.ino
+++ breathing_exercise.ino
@@ -2,10 +2,10 @@ void breathing_exercise(int buzzer)  {
   int notes[] = { 1915, 1700};
   //Serial.println("Breathing Exercise"); 
   for(int aa = 0; aa < 3; aa++)  {
-    tone(buzzer,notes[0],4000);
-    delay(11000);
+    tone(buzzer,notes[0],2000);
+    delay(7500);
     
-    tone(buzzer,notes[1],8000);
+    tone(buzzer,notes[1],4000);
     delay(8000);
   
   } 
--- trigger_wakeup.ino
+++ trigger_wakeup.ino
@@ -1,7 +1,8 @@
 void trigger_wakeup(int buzzer)  {
-  int notes[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  //int notes[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  int notes[] = {1275, 1136, 1915, 1700};
   //Serial.println("Trigger Wakeup"); 
- for(int aa = 0; aa < 8; aa ++)  {
+ for(int aa = 0; aa < 4; aa ++)  {
    tone(buzzer,notes[aa],1000);
    delay(2000);
  }
